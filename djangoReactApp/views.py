7005e08d543b6ab723887aeb57ec1687ba650278
BonyChops <contact@bonychops.com>
2024-06-03 03:05:46 +0900

    feat: djangoロゴを増やす

--- djangoReactApp/views.py
+++ djangoReactApp/views.py
@@ -1,6 +1,16 @@
+import django
 from inertia import inertia
 
 
 @inertia('Index/index')  # 描画Reactコンポーネントの選択
 def index(request):
-    return {}  # 空Props
+    return {
+        "services": [
+            {
+                "version": django.get_version(),
+                "name": "Django",
+                "url": "https://www.djangoproject.com/",
+                "iconUrl": "https://github.com/django.png"
+            }
+        ]
+    }
